44ded6efb45029c72a7ea5ab0a7376df4dae5519
Mayekarp <mayekar.p@husky.neu.edu>
2015-10-02 23:50:50 -0400

    PrimeNumb

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -1,14 +1,43 @@
-﻿namespace FindingPrimeNumbers
+﻿using System;
+
+namespace FindingPrimeNumbers
 {
 	public class FindingPrimeNumbers
 	{
-		public long FindSumOfPrimeNumbers(int n)
-		{
-			long sum = 0;
+	public long FindSumOfPrimeNumbers(int num)
+	{
+            
+            long sum = 0;
+            int j;
+            j = 1;
+            int count = 0;
+            int k;
+            while (count != num)
+            {
+                k = 0;
+                for (int i = 1; i <= j; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        k++;
+                    }
+                }
+                if (k == 2)
+                {
+                    Console.WriteLine("This is a prime number {0}", j);
+                    count = count + 1;
+                    sum = sum + j;
+                }
+                else
+                {
+                    Console.WriteLine("This is not a prime number {0}", j);
+                }
+                j++;
+            }
+            Console.WriteLine("sum =  {0}", sum);
 
-			// your solution goes in here
-
-			return sum;
-		}
+            Console.ReadLine();
+            return sum;
+	    }
 	}
 }
--- FindingPrimeNumbers/Program.cs
+++ FindingPrimeNumbers/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace FindingPrimeNumbers
+﻿using System; 
+
+namespace FindingPrimeNumbers
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-		}
+     public static void Main(string[] args)
+{
+            Console.Write("Enter a Number:");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            FindingPrimeNumbers fp = new FindingPrimeNumbers();
+            long sum = fp.FindSumOfPrimeNumbers(num);
+             
+
+            Console.WriteLine(+sum);
+
+           
+}
 	}
 }
-																																																		
+	
