3363c1c50993a5984924bccde1ac0935f0a31fde
Mayekarp <mayekar.p@husky.neu.edu>
2015-10-08 03:35:57 -0400

    GasPump

--- GasPump/GasPump/Program.cs
+++ GasPump/GasPump/Program.cs
@@ -15,15 +15,64 @@ namespace GasPump
 
 		static void Main(string[] args)
 		{
-			// your implementation here
-		}
+            Console.WriteLine("Enter purchase gas type");
+            string userInput = Console.ReadLine();
+
+            
+            if (UserEnteredSentinelValue(userInput))
+            {
+                Console.WriteLine("Application Terminated");
+                Console.ReadLine();
+
+                System.Environment.Exit(1);
+            }
+
+            if (!UserEnteredValidGasType(userInput))
+            {
+                
+                Console.Write("Invalid input for gas type.");
+                System.Environment.Exit(1);
+
+            }
+           
+            Console.WriteLine("Please enter purchased gas amount");
+            String gasAmount = Console.ReadLine();
+
+            if (UserEnteredValidAmount(gasAmount))
+            {
+                Console.WriteLine("The valued entered is:" +gasAmount);
+                Console.ReadLine();
+                GasType gas = GasTypeMapper(userInput[0]);
+                Console.ReadLine();
+
+                double totalCost = 0.0;
+                CalculateTotalCost(gas,Convert.ToInt32(gasAmount), ref totalCost);
+                Console.WriteLine("TotalCost:" + totalCost);
+                Console.ReadLine();
+
+            }
+
+            else
+            {
+                Console.WriteLine("Entered Input is invalid");
+                System.Environment.Exit(1);
+                Console.ReadLine();
+            }
+            
+            
+        
+    }
 
 		// use this method to check and see if sentinel value is entered
 		public static bool UserEnteredSentinelValue(string userInput)
 		{
 			var result = false;
-
-			// your implementation here
+            
+            if( userInput.Equals("Q")|| userInput.Equals("q"))
+            {
+                result = true;
+            }
+            
 
 			return result;
 		}
@@ -34,7 +83,29 @@ namespace GasPump
 		{
 			var result = false;
 
-			// your implementation here
+			if( userInput.Equals("R") || userInput.Equals("r"))
+            {
+                Console.Write("The entered type is Regular Gas");
+                result = true;
+            }
+
+            else if ( userInput.Equals("M") || userInput.Equals("m"))
+            {
+                Console.Write("The entered type is MidGrade Gas");
+                result = true;
+            }
+
+            else if (userInput.Equals("P") || userInput.Equals("p"))
+            {
+                Console.Write("The entered type is Premium Gas");
+                result = true;
+            }
+
+            else if (userInput.Equals("D") || userInput.Equals("d"))
+            {
+                Console.Write("The entered type is Diesel Gas");
+                result = true;
+            }
 			
 			return result;
 		}
@@ -44,8 +115,9 @@ namespace GasPump
 		public static bool UserEnteredValidAmount(string userInput)
 		{
 			var result = false;
-
-			// your implementation here
+            double value;
+            result = double.TryParse(userInput, out value);
+            
 
 			return result;
 		}
@@ -55,25 +127,100 @@ namespace GasPump
 		// this mapping "must" be used within CalculateTotalCost() method later on
 		public static GasType GasTypeMapper(char c)
 		{
-			GasType gasType = GasType.None;
+            GasType gasType_RegularGas = GasType.RegularGas;
+            GasType gasType_MidGradeGas = GasType.MidgradeGas;
+            GasType gasType_PremiumGas = GasType.PremiumGas;
+            GasType gasType_DieselFuel = GasType.DieselFuel;
+            GasType gasType_None = GasType.None;
 
-			// your implementation here
+            switch (c)
+            {
+                case 'R':
+                case 'r':
+                    {
+                        //GasType gasType = GasType.RegularGas;
+                        return gasType_RegularGas;
 
-			return gasType;
-		}
+                    }
+                   case 'M':
+                    case 'm':
+                    {
+                        //GasType gasType = GasType.MidgradeGas;
+                        return gasType_MidGradeGas;
+                    }
+
+                    case 'P':
+                    case 'p':
+                    {
+                        //GasType gasType = GasType.PremiumGas;
+                        return gasType_PremiumGas;
+
+                    }
+
+                    case 'D':
+                    case 'd':
+                    {
+                        //GasType gasType = GasType.DieselFuel;
+                        return gasType_DieselFuel;
+                    }      
+
+            }
+
+            //GasType gasType = GasType.None;
+                return gasType_None;
+            
+        }
 
 		public static double GasPriceMapper(GasType gasType)
 		{
 			var result = 0.0;
+            switch (gasType)
+            {
+                case GasType.None:
+                    {
+                        result = 0;
+                        break;
+                    }
+                        
+                case GasType.RegularGas:
+                    {
+                        result = 1.94;
+                        break;
+                    }
+                case GasType.MidgradeGas:
+                    {
+                        result = 2.00;
+                        break;
+                    }
+                    
+                case GasType.PremiumGas:
+                    {
+                        result = 2.24;
+                        break;
+                    }
+                    
+                case GasType.DieselFuel:
+                    {
+                        result = 2.17;
+                        break;
+                    }
+                    
+            }
 
-			// your implementation here
-
-			return result;
+            return result;
 	}
 
 		public static void CalculateTotalCost(GasType gasType, int gasAmount, ref double totalCost)
 		{
-			// your implementation here
+
+            double price;
+            price = GasPriceMapper(gasType);
+            Console.WriteLine("The price for gas is - " + price);
+            Console.ReadLine();
+            totalCost = gasAmount * price;
+            
 		}
+
+        
 	}
 }
