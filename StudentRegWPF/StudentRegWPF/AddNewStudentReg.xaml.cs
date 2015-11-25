76bb54891bed0c2b9c6504c2d70fb0f4d1901666
Mayekarp <mayekar.p@husky.neu.edu>
2015-11-24 23:57:24 -0500

    Version 3

---
+++ StudentRegWPF/StudentRegWPF/AddNewStudentReg.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace StudentRegWPF
+{
+    /// <summary>
+    /// Interaction logic for AddNewStudentReg.xaml
+    /// </summary>
+    public partial class AddNewStudentReg : Window
+    {
+        public AddNewStudentReg()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            DBconnection objcon = new DBconnection();
+            objcon.connection();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("Insert into Student_Info (StudentID, Firstname, Lastname, Department, Enrollment_Type) values (@StudentID, @Firstname, @Lastname, @Department, '" + Enrollment_Type + "')", objcon.con);
+                cmd.Parameters.Add("@StudentID", Student_Id.Text);
+                cmd.Parameters.Add("@Firstname", First_Name1.Text);
+                cmd.Parameters.Add("@Lastname", Last_Name1.Text);
+                cmd.Parameters.Add("@Department", Department1.SelectedText.ToString());
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                if (Student_Id.Text == "" || First_Name1.Text == "" || Last_Name1.Text == "")
+                    MessageBox.Show("Please Fill in all the Tables");
+
+            }
+          
+
+            MessageBox.Show("Sucessfully Added New Student");
+            this.Hide();
+            NewStudentRegistration nsr = new NewStudentRegistration();
+            nsr.Show();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Student_Id.Clear();
+            First_Name1.Clear();
+            Last_Name1.Clear();
+            //comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+        }
+    }
+}
--- StudentRegWPF/StudentRegWPF/NewStudentRegistration.xaml.cs
+++ StudentRegWPF/StudentRegWPF/NewStudentRegistration.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace StudentRegWPF
 {
@@ -19,14 +21,66 @@ namespace StudentRegWPF
     /// </summary>
     public partial class NewStudentRegistration : Window
     {
+        
+       
         public NewStudentRegistration()
         {
             InitializeComponent();
+            DBconnection objcon = new DBconnection();
+            objcon.connection();
+            SqlDataAdapter sda = new SqlDataAdapter("Select * from Student_Info", objcon.con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGrid.ItemsSource = dt.DefaultView;
+            objcon.con.Close();
         }
-
+        
         private void Add_Student_Click(object sender, RoutedEventArgs e)
         {
+            this.Hide();
+            AddNewStudentReg ans = new AddNewStudentReg();
+            ans.Show();
 
         }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Remove_Student_Click(object sender, RoutedEventArgs e)
+        {
+            //if (this.dataGridView1.SelectedRows.Count > 0)
+            //{
+            //    this.Hide();
+            //    RemoveStudentReg rsr = new RemoveStudentReg();
+
+            //    string test1 = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value.ToString();
+            //    rsr.TextBoxValue1 = test1;
+            //    string test2 = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[1].Value.ToString();
+            //    rsr.TextBoxValue2 = test2;
+            //    string test3 = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[2].Value.ToString();
+            //    rsr.TextBoxValue3 = test3;
+            //    rsr.Show();
+            }
+
+        private void Edit_Student_Click(object sender, RoutedEventArgs e)
+        {
+            //if (this.dataGridView1.SelectedRows.Count > 0)
+            //{
+            //    this.Hide();
+            //    EditStudentReg esr = new EditStudentReg();
+
+            //    string test1 = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value.ToString();
+            //    esr.TextBoxValue1 = test1;
+            //    string test2 = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[1].Value.ToString();
+            //    esr.TextBoxValue2 = test2;
+            //    string test3 = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[2].Value.ToString();
+            //    esr.TextBoxValue3 = test3;
+            //    esr.Show();
+            //}
+        
     }
+    }
+
 }
